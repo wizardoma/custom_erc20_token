5f32d785c36d2da55139fc476ddb179cb1bcc5e4
Ibekason Alexander <alibekason@gmail.com>
2022-07-25 17:48:18 +0100

    test: Add eventfactory tests

--- test/eventfactory.test.js
+++ test/eventfactory.test.js
@@ -1,45 +1,64 @@
 const Web3Bets = artifacts.require("Web3Bets");
 const EventFactory = artifacts.require("EventFactory");
-const MarketFactory = artifacts.require("MarketFactory");
 const truffleAssert = require("truffle-assertions");
 
-let web3bets;
 let eventFactory;
-let marketFactory;
-let marketFactoryAddress;
-let accounts;
+let web3bets;
+let wAddress;
 
 beforeEach(async () => {
-    web3bets = await Web3Bets.deployed();
-  
-    accounts = await web3.eth.getAccounts();
+  web3bets = Web3Bets.deployed();
 
-    marketFactory = await MarketFactory.deployed();
+  wAddress = await web3bets.address;
 
-    marketFactoryAddress = await marketFactory.address;
-  
-    eventFactory = await EventFactory.deployed();
-});  
+  accounts = await web3.eth.getAccounts();
+
+  eventFactory = await EventFactory.deployed();
+});
 
 contract("EventFactory", (accounts) => {
+  let eventOwner = accounts[1];
 
-    it("Should have no events on initialization", async () => {
-        let events= await eventFactory.getAllEvents();
 
-        assert.equal(events.length,0)
+  it("Should be able to create to create an event", async () => {
+    let allEvents = await eventFactory.getAllEvents({ from: eventOwner });
+
+    let event = await eventFactory.createEvent("Man U vs Aston Villa", 1, {
+      from: eventOwner,
     });
 
-    it("Should be able to create to create an event", async () => {
-        let allEvents= await eventFactory.getAllEvents();
+    console.log(event);
 
-        let event = await eventFactory.createEvent("Man U vs Aston Villa",1);
+    // check length of eventFactory
+    let newEvents = await eventFactory.getAllEvents({ from: eventOwner });
+    assert.equal(newEvents.length - allEvents.length === 1, true);
+  });
 
-        console.log(event);
 
-        // check length of eventFactory
-        let newEvents =  await eventFactory.getAllEvents();
-        assert.equal(newEvents.length - allEvents.length === 1,true)
-        
+  it("Event owner should only get events they created", async () => {
+    
+    let eventName1 = "Man U vs Aston Villa";
+    let eventName2= "Man U vs Chelsea";
+
+    let ownerEvents = await eventFactory.getEventsByAddress(eventOwner);
+    let otherEvents = await eventFactory.getEventsByAddress(accounts[0]);
+
+
+    var event1Created = await eventFactory.createEvent( eventName1,1, {
+      from: eventOwner,
     });
 
-})
+
+    var event2Created = await eventFactory.createEvent(eventName2, 1, {
+      from: accounts[0],
+    });
+
+
+    let newOwnerEvents = await eventFactory.getEventsByAddress(eventOwner);
+    let newOtherEvents = await eventFactory.getEventsByAddress(accounts[0]);
+
+    assert.equal(newOtherEvents - otherEvents ===1 && newOwnerEvents - ownerEvents === 1, true)
+    
+
+  });
+});
